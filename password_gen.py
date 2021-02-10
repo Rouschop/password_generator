2569d5d115b909f25cceb59c6efecc2ff38fee6e
Jordy Rouschop <j.rouschop@outlook.com>
2021-02-10 02:58:39 -0500

    PEP style

--- password_gen.py
+++ password_gen.py
@@ -26,39 +26,41 @@ wordlist = args.wordlist
 
 
 # Colors for error message
-class colors:
-    OK = '\033[92m' #GREEN
-    WARNING = '\033[93m' #YELLOW
-    FAIL = '\033[91m' #RED
+class Colors:
+    OK = '\033[92m'  # GREEN
+    WARNING = '\033[93m'  # YELLOW
+    FAIL = '\033[91m'  # RED
 
 
-def contains_digit(string):  # Regex to check if word contains a numeral
+def contains_digit(string):
+    """Regex to check if word contains a numeral"""
     re_digit = re.compile(r'\d')
     if re.search(re_digit, string):
         return True
 
 
-def contains_quotes(string):  # Regex to check if word contains apostrophe or a dash
-    re_symbol = re.compile(r"'|-")
+def contains_quotes(string):
+    """Regex to check if word contains apostrophe or a dash"""
+    re_symbol = re.compile(r"['\-]")
     if re.search(re_symbol, string):
         return True
 
 
-def make_wordlist(read_lines=None):  # Makes a list of words to use for password generation
+def make_wordlist():
+    """Makes a list of words to use for password generation"""
     global minimum
     global maximum
-    if read_lines is None:
-        read_lines = new_line
     if minimum or maximum is None:
         minimum, maximum = 6, 9
-    for line in read_lines:
+    for line in new_line:
         if not contains_quotes(line) and not contains_digit(line):  # Check if no quotes, dash and digits in line
             if minimum < len(line) <= maximum:
                 if line.islower():
                     cleaned_list.append(line[:-1])  # Append line to list without \n character
 
 
-def choose_random():  # Choose the words for the password randomly with secrets
+def choose_random():
+    """Choose the words for the password randomly with secrets"""
     global words
     if words is None:
         words = 4
@@ -68,7 +70,9 @@ def choose_random():  # Choose the words for the password randomly with secrets
     return password
 
 
-def print_passwords(count):  # Print the password with one white space between each word
+def print_passwords():
+    """Print the password with one white space between each word"""
+    global count
     if count is None:
         count = 1
     for i in range(count):
@@ -78,23 +82,23 @@ def print_passwords(count):  # Print the password with one white space between e
 if __name__ == '__main__':
     try:
         if wordlist is None:
-            if path.exists('wordlist.txt'):  # If None wordlist is specified check if wordlist.txt exists
+            if path.exists('wordlist.txt'):  # If wordlist is not specified check if wordlist.txt exists
                 wordlist = 'wordlist.txt'
-            else:  # Raise FileNotFound error if no wordlist is specified and wordlist.txt doesnt exist
-                print(colors.FAIL, "Wordlist not found, please specify a wordlist.")
+            else:  # Raise FileNotFound error if no wordlist is specified and wordlist.txt does not exist
+                print(Colors.FAIL, "Wordlist not found, please specify a wordlist.")
                 raise FileNotFoundError(
                     errno.ENOENT, os.strerror(errno.ENOENT), wordlist)
-        if wordlist is not None:
+        else:  # Else check if path to wordlist is valid
             if path.exists(wordlist):
-                with open(wordlist) as word_list:
+                with open(wordlist) as word_list:  # Open wordlist; read lines; make a list of words; print password
                     new_line = word_list.readlines()
                     cleaned_list = []
                     make_wordlist()
-                    print_passwords(count)
+                    print_passwords()
             else:
-                print(colors.FAIL, "Path to wordlist is invalid, please specify a valid wordlist")
+                print(Colors.FAIL, "Path to wordlist is invalid, please specify a valid wordlist")
                 raise FileNotFoundError(
                     errno.ENOENT, os.strerror(errno.ENOENT), wordlist)
     except Exception as e:
-        print(colors.FAIL, "Unknown error, please report to ")
+        print(Colors.FAIL, "Unknown error, please report to rouschop96@gmail.com")
         print(e)
