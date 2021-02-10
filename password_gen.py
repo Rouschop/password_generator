03718a9ac235977a18568a1f8373d462ef5de57b
Jordy Rouschop <j.rouschop@outlook.com>
2021-02-10 05:50:01 -0500

    Added docstring

--- password_gen.py
+++ password_gen.py
@@ -25,8 +25,8 @@ count = args.count
 wordlist = args.wordlist
 
 
-# Colors for error message
 class Colors:
+    """Colors for error message"""
     OK = '\033[92m'  # GREEN
     WARNING = '\033[93m'  # YELLOW
     FAIL = '\033[91m'  # RED
